7cc74cbfad0533008e33f4d496e62ca5e907c4c2
pwnerblu <netcomusicinquiries@gmail.com>
2025-09-28 08:23:50 -0400

    Add option to pwn with Mac or iOS device

--- surrealra1n.sh
+++ surrealra1n.sh
@@ -337,7 +337,15 @@ case "$1" in
         echo "[*] Restoring to iOS $IOS_VERSION..."
         restoredir="restorefiles/$IDENTIFIER/$IOS_VERSION"
         echo "first, your device needs to be in pwndfu mode. pwning with gaster"
-        ./bin/gaster pwn
+        echo "[!] Linux has low success rate for the checkm8 exploit on A6-A7. If possible, you should connect your device to a Mac or iOS device and pwn with ipwnder"
+        read -p "[!] Do you want to continue pwning with gaster? (LOW SUCCESS RATE) y/n " response
+        if [[ $response == y ]]; then
+            ./bin/gaster pwn
+        else
+            echo "Now, disconnect your device and connect it to a Mac or iOS device to pwn with ipwnder."
+            echo "For more information about pwning with an iOS device, go to <https://github.com/LukeZGD/Legacy-iOS-Kit/wiki/Pwning-Using-Another-iOS-Device>"
+            read -p "Press any key after the device is pwned with ipwnder and reconnected to this computer"
+        fi
         ./bin/gaster reset
         echo "[*] Verifying PWNDFU mode..."
         irecovery_output=$(./bin/irecovery -q)
@@ -390,7 +398,15 @@ case "$1" in
         IPSW="$2"
         echo "[*] Restoring to iOS 10.3.3..."
         echo "first, your device needs to be in pwndfu mode. pwning with gaster"
-        ./bin/gaster pwn
+        echo "[!] Linux has low success rate for the checkm8 exploit on A6-A7. If possible, you should connect your device to a Mac or iOS device and pwn with ipwnder"
+        read -p "[!] Do you want to continue pwning with gaster? (LOW SUCCESS RATE) y/n " response
+        if [[ $response == y ]]; then
+            ./bin/gaster pwn
+        else
+            echo "Now, disconnect your device and connect it to a Mac or iOS device to pwn with ipwnder."
+            echo "For more information about pwning with an iOS device, go to <https://github.com/LukeZGD/Legacy-iOS-Kit/wiki/Pwning-Using-Another-iOS-Device>"
+            read -p "Press any key after the device is pwned with ipwnder and reconnected to this computer"
+        fi
         ./bin/gaster reset
         echo "[*] Verifying PWNDFU mode..."
         irecovery_output=$(./bin/irecovery -q)
@@ -438,7 +454,15 @@ case "$1" in
         read -p "What is the iOS version you are downgrading to: " vers
         echo "[*] Restoring to iOS $vers..."
         echo "first, your device needs to be in pwndfu mode. pwning with gaster"
-        ./bin/gaster pwn
+        echo "[!] Linux has low success rate for the checkm8 exploit on A6-A7. If possible, you should connect your device to a Mac or iOS device and pwn with ipwnder"
+        read -p "[!] Do you want to continue pwning with gaster? (LOW SUCCESS RATE) y/n " response
+        if [[ $response == y ]]; then
+            ./bin/gaster pwn
+        else
+            echo "Now, disconnect your device and connect it to a Mac or iOS device to pwn with ipwnder."
+            echo "For more information about pwning with an iOS device, go to <https://github.com/LukeZGD/Legacy-iOS-Kit/wiki/Pwning-Using-Another-iOS-Device>"
+            read -p "Press any key after the device is pwned with ipwnder and reconnected to this computer"
+        fi
         ./bin/gaster reset
         echo "[*] Verifying PWNDFU mode..."
         irecovery_output=$(./bin/irecovery -q)
@@ -568,7 +592,16 @@ case "$1" in
 
         # Placeholder for tethered boot command
         echo "[*] Proceeding to tethered boot..."
-        ./bin/gaster pwn
+        echo "first, your device needs to be in pwndfu mode. pwning with gaster"
+        echo "[!] Linux has low success rate for the checkm8 exploit on A6-A7. If possible, you should connect your device to a Mac or iOS device and pwn with ipwnder"
+        read -p "[!] Do you want to continue pwning with gaster? (LOW SUCCESS RATE) y/n " response
+        if [[ $response == y ]]; then
+            ./bin/gaster pwn
+        else
+            echo "Now, disconnect your device and connect it to a Mac or iOS device to pwn with ipwnder."
+            echo "For more information about pwning with an iOS device, go to <https://github.com/LukeZGD/Legacy-iOS-Kit/wiki/Pwning-Using-Another-iOS-Device>"
+            read -p "Press any key after the device is pwned with ipwnder and reconnected to this computer"
+        fi
         ./bin/gaster reset
         echo "[*] Verifying PWNDFU mode..."
         irecovery_output=$(./bin/irecovery -q)
