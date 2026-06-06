9f92d2ac1b38c7f5c35051255caf2299171ea162
pwnerblu <219584518+pwnerblu@users.noreply.github.com>
2026-06-06 17:51:40 -0400

    v1.3.23

--- surrealra1n.sh
+++ surrealra1n.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CURRENT_VERSION="v1.3.22"
+CURRENT_VERSION="v1.3.23"
 
 clear
 
@@ -582,9 +582,9 @@ echo "Downloading wikiproxy"
 curl -L -o wikiproxy.py https://github.com/tihmstar/libipatcher/raw/refs/heads/master/wikiproxy.py
 
 if [[ $USE_WIKIPROXY == 1 ]]; then
-    PID=$(lsof -ti:8888)
-    if [[ -n "$PID" ]]; then
-        kill -9 $PID
+    PID=$(lsof -ti:8888) || true
+    if [[ -n "$PID" ]]; then || true
+        kill -9 $PID || true
     fi
     python3 wikiproxy.py &
 fi
