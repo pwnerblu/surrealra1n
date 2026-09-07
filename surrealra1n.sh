6c123beb96c10bf8592b44f2c99585868274c27f
afastaudir8 <45905959+afastaudir8@users.noreply.github.com>
2026-09-07 13:30:44 -0400

    Allow choosing MP instead on any version

--- surrealra1n.sh
+++ surrealra1n.sh
@@ -212,12 +212,12 @@ if [[ $dist == 3 || $dist == 4 ]]; then
 	    darwin_package_manager=1
         if [[ "$(printf '%s\n' "14.0" "$macos_ver" | sort -V | head -n1)" == "14.0" ]]; then
             echo "Using Homebrew is not recommended on your macOS version ($macos_ver)."
-            if command -v port &>/dev/null; then
-                read -r -p "Use MacPorts instead? [y/n]" yn
-                if [[ $yn =~ ^[yY] ]]; then
-                    darwin_package_manager=2
-                fi
-            fi
+        fi
+        if command -v port &>/dev/null; then
+            read -r -p "Use MacPorts instead? [y/n]" yn
+            if [[ $yn =~ ^[yY] ]]; then
+                darwin_package_manager=2
+        fi
     elif command -v port &>/dev/null; then
 	    echo "Using MacPorts"
 	    darwin_package_manager=2
