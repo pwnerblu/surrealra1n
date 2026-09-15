cea2b15f2dd1381f758984c2c61d8d514c33dec1
pwnerblu <219584518+pwnerblu@users.noreply.github.com>
2026-09-15 00:15:41 -0400

    some other fix

--- surrealra1n.sh
+++ surrealra1n.sh
@@ -225,7 +225,7 @@ if [[ $dist == 3 || $dist == 4 ]]; then
         darwin_package_manager=2
     else
         echo "No package manager installed. Please install Homebrew or MacPorts."
-        if [[ $dist == 3 ]] && [[ "$(printf '%s\n' "$BREW_MIN" "$macos_ver" | sort -V | head -n1)" != "$BREW_MIN" ]]; then
+        if [[ $dist == 3 ]] && [[ "$(printf '%s\n' "$BREW_MIN" "$macos_ver" | sort -V | head -n1)" == "$BREW_MIN" ]]; then
             echo "Homebrew is recommended on this Mac: https://brew.sh"
             echo "You can also use MacPorts: https://macports.org"
         else
